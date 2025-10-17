edb663039c78be22165342d1e9fa7fee9476cbf9
Nicolas <nicolas.cataluna@gmail.com>
2025-10-17 23:25:10 +0200

    upd

--- assets/app.js
+++ assets/app.js
@@ -4,3 +4,5 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import './styles/app.css';
 import './controllers/animation_controller.js';
+import './controllers/hello_controller.js';
+import './controllers/csrf_protection_controller.js';
